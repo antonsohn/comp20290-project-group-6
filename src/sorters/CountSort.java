82b5febd81ec9bf5e87c09e1194fc8813667a9a7
Anton Sohn <anton.sohn1@gmail.com>
2026-04-10 12:28:59 +0100

    Moved BubbleSort.java and CountSort.java to the correct repo.

---
+++ src/sorters/CountSort.java
@@ -0,0 +1,30 @@
+package sorters;
+
+import java.util.Arrays;
+
+public class CountSort {
+    public static int[] sort(int[] array){
+
+        int[] sorted = new int[array.length];
+        int[] numbers = new int[Arrays.stream(array).max().getAsInt() + 1];
+        Arrays.fill(numbers, 0);
+
+        for (int j : array) {
+            numbers[j]++;
+        }
+
+        for(int i = 1; i< numbers.length; i++){
+            numbers[i] = numbers[i] + numbers[ i-1];
+        }
+
+        for(int i = array.length - 1; i >= 0; i--){
+            sorted[numbers[array[i]]-1] = array[i];
+            numbers[array[i]] --;
+        }
+
+        return sorted;
+
+    }
+
+
+}
